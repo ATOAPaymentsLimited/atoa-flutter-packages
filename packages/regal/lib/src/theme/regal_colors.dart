dfee7e3d656bc969f16ee93eb8a7ff4f8cb6122a
Aman Atoa <aman@paywithatoa.co.uk>
2022-11-17 10:51:57 +0530

    feat: add a color

--- packages/regal/lib/src/theme/regal_colors.dart
+++ packages/regal/lib/src/theme/regal_colors.dart
@@ -8,6 +8,7 @@ class RegalColors {
   static const yankeesBlue = Color(0xFF0F2649);
   static const indigo = Color(0xFF15376A);
   static const azureishWhite = Color(0xFFD2DDF1);
+  static const celticBlue = Color(0xFF2767C6);
   static const green = Green();
   static const red = Red();
   static const gold = Gold();
