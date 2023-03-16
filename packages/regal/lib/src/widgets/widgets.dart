beba7484e1310e341d3a902e0486051ac4654f94
Tushar Gupta <tushar@paywithatoa.co.uk>
2023-03-17 00:13:57 +0530

    feat(click-tracking): Added custom text button widget

--- packages/regal/lib/src/widgets/widgets.dart
+++ packages/regal/lib/src/widgets/widgets.dart
@@ -1,5 +1,6 @@
 export 'carousel_slider/carousel_slider.dart';
 export 'custom_navigation_card.dart';
+export 'custom_text_button.dart';
 export 'focus_lines_shape_animation.dart';
 export 'progress_timeline.dart';
 export 'supported_bank_sliders.dart';
