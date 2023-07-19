1bca7f8463c9941b015bcf6192951ad57102b63e
Aman Atoa <aman@paywithatoa.co.uk>
2023-07-19 12:14:11 +0530

    fix: inter text color

--- packages/regal/lib/src/theme/text/inter_text_theme.dart
+++ packages/regal/lib/src/theme/text/inter_text_theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:regal/regal.dart';
 
 final kInterTextTheme = GoogleFonts.interTextTheme(
   TextTheme(
@@ -79,5 +80,8 @@ final kInterTextTheme = GoogleFonts.interTextTheme(
       fontSize: 11.sp,
       fontWeight: FontWeight.w400,
     ),
+  ).apply(
+    bodyColor: RegalColors.licoriceBlack,
+    displayColor: RegalColors.licoriceBlack,
   ),
 );
