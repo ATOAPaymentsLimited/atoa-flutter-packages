e62ae5ec0887c70c09c8ab5d07d13f79426ab1b2
Chirag <52071686+alphapokedex@users.noreply.github.com>
2023-08-14 07:59:17 +0530

    fix: height + font weight ext

--- packages/regal/lib/src/utils/text_style_extension.dart
+++ packages/regal/lib/src/utils/text_style_extension.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 extension TextStyleExtension on TextStyle {
-  TextStyle height(double value) => copyWith(height: value);
+  TextStyle get height130 => copyWith(height: 1.3);
+  TextStyle get height150 => copyWith(height: 1.5);
 
-  TextStyle weight(FontWeight value) => copyWith(fontWeight: value);
+  TextStyle get w400 => copyWith(fontWeight: FontWeight.w400);
+  TextStyle get w600 => copyWith(fontWeight: FontWeight.w600);
+  TextStyle get w700 => copyWith(fontWeight: FontWeight.w700);
 
   TextStyle size(double value) => copyWith(fontSize: value);
 }
