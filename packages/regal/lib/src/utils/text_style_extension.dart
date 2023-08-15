2dfa82edf9a8cbcfab2f319c49dd14eaee017d99
Chirag <52071686+alphapokedex@users.noreply.github.com>
2023-08-16 05:08:56 +0530

    feat: w300

--- packages/regal/lib/src/utils/text_style_extension.dart
+++ packages/regal/lib/src/utils/text_style_extension.dart
@@ -4,6 +4,7 @@ extension TextStyleExtension on TextStyle {
   TextStyle get height130 => copyWith(height: 1.3);
   TextStyle get height150 => copyWith(height: 1.5);
 
+  TextStyle get w300 => copyWith(fontWeight: FontWeight.w300);
   TextStyle get w400 => copyWith(fontWeight: FontWeight.w400);
   TextStyle get w600 => copyWith(fontWeight: FontWeight.w600);
   TextStyle get w700 => copyWith(fontWeight: FontWeight.w700);
