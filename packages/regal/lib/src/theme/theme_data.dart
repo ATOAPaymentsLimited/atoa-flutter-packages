3aba639a480ceb7d028305f63eff1e596462fb94
Yash Tiwari <yasht.2701@gmail.com>
2023-08-17 10:07:10 +0530

    refactor: remove 100, 200, 300 weights and add 600 weight

--- packages/regal/lib/src/theme/theme_data.dart
+++ packages/regal/lib/src/theme/theme_data.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:regal/regal.dart';
 import 'package:regal/src/theme/theme.dart';
 
