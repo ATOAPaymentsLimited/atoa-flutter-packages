8e6a6d87d32474a6d20cf3fe97c992eec429e989
Chirag <52071686+alphapokedex@users.noreply.github.com>
2023-11-19 11:24:12 +0530

    fix: icon button container size

--- packages/regal/lib/src/widgets/regal_icon_button.dart
+++ packages/regal/lib/src/widgets/regal_icon_button.dart
@@ -61,8 +61,6 @@ class RegalIconButton extends StatelessWidget {
       onTap: () => onPressed?.call(context),
       behavior: HitTestBehavior.translucent,
       child: Container(
-        height: size.value,
-        width: size.value,
         padding: padding,
         decoration: BoxDecoration(
           color: color?.withOpacity(disabled ? 0.3 : bgOpacity ?? 1.0),
