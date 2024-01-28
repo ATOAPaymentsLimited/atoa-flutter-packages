e4491a9056d3aaa0ccce6c5d815b55f936a34a7a
Aman Nanda <aman+github@paywithatoa.co.uk>
2024-01-28 15:47:11 +0530

    feat: example app

---
+++ packages/face_detector_view/example/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:face_detector_view/face_detector_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => FaceDetectorView(
+                    onValidatedImageCapture: (file) {
+                      print(file.path);
+
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              );
+            },
+            child: const Text('Open Face View')),
+      ),
+    
+    );
+  }
+}
