454ad47dc988428639f39869a315bcbf2d12ca43
Chirag <52071686+alphapokedex@users.noreply.github.com>
2024-10-18 12:47:58 +0530

    style: Add FigTree fonts

--- packages/regal/lib/src/utils/text_style_extension.dart
+++ packages/regal/lib/src/utils/text_style_extension.dart
@@ -4,7 +4,10 @@ extension TextStyleExtension on TextStyle {
   TextStyle get height011 => copyWith(height: 0.11);
   TextStyle get height012 => copyWith(height: 0.12);
   TextStyle get height130 => copyWith(height: 1.3);
+  TextStyle get height140 => copyWith(height: 1.4);
+  TextStyle get height145 => copyWith(height: 1.45);
   TextStyle get height150 => copyWith(height: 1.5);
+  TextStyle get height160 => copyWith(height: 1.6);
 
   TextStyle get w300 => copyWith(fontWeight: FontWeight.w300);
   TextStyle get w400 => copyWith(fontWeight: FontWeight.w400);
