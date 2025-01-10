c5c7dd80d2a6b5f199488a3f20b1431b4e0c02ea
Yash Tiwari <yasht.me@gmail.com>
2025-01-10 16:57:28 +0530

    feat: update carousel item widget to include optional widget parameter

--- packages/regal/lib/src/widgets/carousel_slider/carousel_item_widget.dart
+++ packages/regal/lib/src/widgets/carousel_slider/carousel_item_widget.dart
@@ -14,7 +14,8 @@ class CarouselItemWidget extends StatelessWidget {
         desc = '',
         bgColor = null,
         textColor = null,
-        prefixIcon = null;
+        prefixIcon = null,
+        widget = null;
 
   const CarouselItemWidget.card({
     super.key,
@@ -26,7 +27,21 @@ class CarouselItemWidget extends StatelessWidget {
     this.onTap,
     this.disableAnimationsForTest = false,
     this.padding,
-  }) : imageUrl = '';
+  })  : imageUrl = '',
+        widget = null;
+
+  const CarouselItemWidget.widget({
+    super.key,
+    this.widget,
+    this.onTap,
+    this.disableAnimationsForTest = false,
+    this.padding,
+  })  : title = '',
+        desc = '',
+        prefixIcon = null,
+        imageUrl = '',
+        bgColor = null,
+        textColor = null;
 
   final String imageUrl;
   final String title;
@@ -37,6 +52,7 @@ class CarouselItemWidget extends StatelessWidget {
   final VoidCallback? onTap;
   final bool disableAnimationsForTest;
   final EdgeInsets? padding;
+  final Widget? widget;
 
   @override
   Widget build(BuildContext context) => Padding(
@@ -44,6 +60,7 @@ class CarouselItemWidget extends StatelessWidget {
         child: Stack(
           fit: StackFit.expand,
           children: [
+            if (widget != null) widget!,
             if (title.isNotEmpty && desc.isNotEmpty && prefixIcon is Widget)
               RegalStatusCard.info(
                 bgColor: bgColor,
