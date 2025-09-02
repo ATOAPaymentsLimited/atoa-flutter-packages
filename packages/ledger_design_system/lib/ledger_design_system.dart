84379c08af99e4c931e3fe2f99ae81afddbebaa2
Chirag <52071686+alphapokedex@users.noreply.github.com>
2025-09-03 00:48:08 +0530

    feat: ledger - chore: removed unnecessary code

--- packages/ledger_design_system/lib/ledger_design_system.dart
+++ packages/ledger_design_system/lib/ledger_design_system.dart
@@ -4,9 +4,3 @@ export 'callbacks/callbacks_handler.dart';
 export 'ledger_theme/ledger_theme.dart';
 export 'spacing/spacing.dart';
 export 'utils/utils.dart';
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
