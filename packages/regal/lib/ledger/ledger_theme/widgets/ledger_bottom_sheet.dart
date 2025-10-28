70a71610703133a92e139436882be44bdab15134
kannan-atoa <kannan@paywithatoa.co.uk>
2025-10-28 17:28:04 +0530

    fix: drag bar parameter fix

--- packages/regal/lib/ledger/ledger_theme/widgets/ledger_bottom_sheet.dart
+++ packages/regal/lib/ledger/ledger_theme/widgets/ledger_bottom_sheet.dart
@@ -378,7 +378,7 @@ Future<T?> showLedgerBottomSheetPageView<T>({
             titleAlign: titleAlign,
             showCloseButton: showCloseButton,
             onClose: onClose,
-            showDragHandle: showDragHandle
+            showDragHandle: showDragHandle,
             titleBottomSpacing: titleBottomSpacing,
             illustrationWidget: illustrationWidget,
             physics: physics,
